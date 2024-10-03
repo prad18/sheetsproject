c3975d1cbdda49c2ffa677762cacdf6f5ce358c8
pradish <pradishvaidyas@gmail.com>
2024-10-03 22:29:27 +0530

    Refactor settings.py to use environment variable for SECRET_KEY

--- sheetsproject/settings.py
+++ sheetsproject/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from pathlib import Path
 from dotenv import load_dotenv
-
+import os
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8#eoig2()symuk-2l55@1h7sh&n*q%+=fw1^x1t8jxgypc$+sl'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
