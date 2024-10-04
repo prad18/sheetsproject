ee7c99df45835c62db2a4b1e5aed5af92423e3c5
pradish <pradishvaidyas@gmail.com>
2024-10-04 12:29:17 +0530

    commit

--- sheetsproject/settings.py
+++ sheetsproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://sheetsproject.onrender.com/","127.0.0.1","localhost"]
+ALLOWED_HOSTS = ["sheetsproject.onrender.com/","127.0.0.1","localhost"]
 
 
 # Application definition
